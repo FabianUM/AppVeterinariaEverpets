24c111d91e6938057a3e7bf22a47f916d14a7fa9
FabianUM <fabian.israel.07@gmail.com>
2024-11-01 15:16:44 -0500

    adjustments in the code of the medical appointments table

--- src/DAO/CRUD_ReservarCita.java
+++ src/DAO/CRUD_ReservarCita.java
@@ -12,16 +12,16 @@ public class CRUD_ReservarCita extends ConectarBD{
     public CRUD_ReservarCita(){}
     
     //metodo que muestra en JTable los registros de las tablas categorias
-    public void MostrarCategoriasEnTablaTurno(JTable tabla, String fecha){
+    public void MostrarCitaEnTablaTurno(JTable tabla, String fecha){
         String[] Titulos={"Nro","Codigo","Fecha","Turno","Estado"};
         DefaultTableModel modelo=new DefaultTableModel(null,Titulos);
         tabla.setModel(modelo);
         Citas cat=new Citas();
         int cantreg=0;
         try{
-            rs=st.executeQuery("select c.idCita,c.FechaCita,c.idTurno,c.idEstado" +
-                              " from CITA c"+
-                              " where c.FechaCita='"+fecha+"';");
+            rs=st.executeQuery("SELECT c.idCita,c.FechaCita,c.idTurno,c.idEstado" +
+                              " FROM CITA c"+
+                              " WHERE c.FechaCita='"+fecha+"';");
             while(rs.next()){
                 cantreg++;
                 cat.setIdCita(rs.getString(1));
@@ -37,7 +37,7 @@ public class CRUD_ReservarCita extends ConectarBD{
     }//fin metodo
     
     //metodo que muestra en JTable los registros de las tablas categorias
-    public void MostrarCategoriasEnTabla(JTable tabla, String consulta){
+    public void MostrarCitaEnTabla(JTable tabla, String consulta){
         String[] Titulos={"Nro","Cita","Propietario","DNI","Mascota","Nombre PET","Servicio","Veterinario","Fecha","Hora","Estado"};
         DefaultTableModel modelo=new DefaultTableModel(null,Titulos);
         tabla.setModel(modelo);
@@ -50,7 +50,7 @@ public class CRUD_ReservarCita extends ConectarBD{
                 cat.setIdCita(rs.getString(1));
                 cat.setNombres(rs.getString(2));
                 cat.setApellidos(rs.getString(3));
-                cat.setDni(rs.getString(4));
+                cat.setIdMascota(rs.getString(4));
                 cat.setIdMascota(rs.getString(5));
                 cat.setNombreM(rs.getString(6));
                 cat.setNombreServicio(rs.getString(7));
@@ -67,13 +67,12 @@ public class CRUD_ReservarCita extends ConectarBD{
     }//fin metodo
     
      //metodo que inserta registros a la tabla
-    public void InsertarCategoria(Citas cat){
+    public void InsertarCita(Citas cat){
         try{
             //preparando la consulta con parametros a travez de los simbolos de interrogante(?)
-            ps=con.prepareStatement("insert into CITA (DNI,idMascota,FechaCita,idTurno,idServicios,idVeterinario,Precio,Descripcion,idEstado)" +
-                                    " values(?,?,?,?,?,?,?,?,1);");
+            ps=con.prepareStatement("INSERT INTO CITA (idMascota,FechaCita,idTurno,idServicios,idVeterinario,Precio,Descripcion,idEstado)" +
+                                    " VALUES(?,?,?,?,?,?,?,1);");
             //actualizando los parametros
-            ps.setString(1, cat.getDni());
             ps.setString(2, cat.getIdMascota());
             
             SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd");
@@ -94,23 +93,22 @@ public class CRUD_ReservarCita extends ConectarBD{
     }//fin metodo
     
     //metodo que recupera un registro de la tabla por medio del id
-    public Citas RecuperarCategoria(String idcat){
+    public Citas RecuperarCita(String idcat){
         Citas cat=null;
         try{
-            rs=st.executeQuery("select c.IdCita, c.DNI, c.idMascota, c.FechaCita, c.idTurno, c.idServicios, c.idVeterinario, c.Precio, c.Descripcion" +
-                              " from CITA c" +
-                              " where c.IdCita='"+idcat+"';");
+            rs=st.executeQuery("SELECT c.IdCita, c.idMascota, c.FechaCita, c.idTurno, c.idServicios, c.idVeterinario, c.Precio, c.Descripcion" +
+                              " FROM CITA c" +
+                              " WHERE c.IdCita='"+idcat+"';");
             if(rs.next()){
                 cat=new Citas();
                 cat.setIdCita(rs.getString(1));
-                cat.setDni(rs.getString(2));
-                cat.setIdMascota(rs.getString(3));
-                cat.setFecha(rs.getDate(4));
-                cat.setIdTurno(rs.getInt(5));
-                cat.setIdServicios(rs.getInt(6));
-                cat.setIdVeterinario(rs.getString(7));
-                cat.setPrecio(rs.getDouble(8));
-                cat.setDescripcion(rs.getString(9));
+                cat.setIdMascota(rs.getString(2));
+                cat.setFecha(rs.getDate(3));
+                cat.setIdTurno(rs.getInt(4));
+                cat.setIdServicios(rs.getInt(5));
+                cat.setIdVeterinario(rs.getString(6));
+                cat.setPrecio(rs.getDouble(7));
+                cat.setDescripcion(rs.getString(8));
             }
             con.close();
         }catch(Exception e){
@@ -122,7 +120,7 @@ public class CRUD_ReservarCita extends ConectarBD{
     //metodo que actualiza un registro de categoria
     public void TerminarCita(Citas cat){
         try{
-            ps=con.prepareStatement("update CITA set idEstado=2 where IdCita=?;");
+            ps=con.prepareStatement("UPDATE CITA SET idEstado=2 WHERE IdCita=?;");
             ps.setString(1, cat.getIdCita());
             ps.executeUpdate();
             Mensajes.M1("Registro actualizado correctamente");
@@ -135,7 +133,7 @@ public class CRUD_ReservarCita extends ConectarBD{
     //metodo que actualiza un registro de categoria
     public void CancelarCita(Citas cat){
         try{
-            ps=con.prepareStatement("update CITA set idEstado=3 where IdCita=?;");
+            ps=con.prepareStatement("UPDATE CITA SET idEstado=3 WHERE IdCita=?;");
             ps.setString(1, cat.getIdCita());
             ps.executeUpdate();
             Mensajes.M1("Registro actualizado correctamente");
@@ -148,7 +146,7 @@ public class CRUD_ReservarCita extends ConectarBD{
     //metodo que actualiza un registro de categoria
     public void ReprogramarCita(Citas cat){
         try{
-            ps=con.prepareStatement("update CITA c set c.FechaCita=?, c.idTurno=? where c.IdCita=?;");
+            ps=con.prepareStatement("UPDATE CITA c SET c.FechaCita=?, c.idTurno=? WHERE c.IdCita=?;");
             SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd");
             ps.setString(1, df.format(cat.getFecha()));
             ps.setInt(2, cat.getIdTurno());
@@ -164,7 +162,7 @@ public class CRUD_ReservarCita extends ConectarBD{
     //metodo que muestra codigo en ticket
     public void CargarCodigoTicket(JLabel codigo){
         try{
-            rs=st.executeQuery("select max(IdCita) as IdCita from CITA;");
+            rs=st.executeQuery("SELECT MAX(idTicket) AS idTicket FROM TICKET;");
             if(rs.next()){
                 String c=rs.getString("IdCita");
                 codigo.setText(c);
@@ -177,13 +175,14 @@ public class CRUD_ReservarCita extends ConectarBD{
     //metodo que muestra datos en ticket
     public void DatosTicket(String Codigo, JLabel j1, JLabel j2){
         try{
-            rs=st.executeQuery("select c.FechaCita, t.hora"+
-                              " from CITA c"+
-                              " inner join TURNO t on c.idTurno=t.idTurno"+
-                              " where c.IdCita='"+Codigo+"';");
+            rs=st.executeQuery("SELECT c.FechaCita, tu.hora"+
+                              " FROM TICKET ti"+
+                              " INNER JOIN CITA c ON ti.IdCita =c.IdCita"+
+                              " INNER JOIN TURNO tu ON c.idTurno=tu.idTurno"+
+                              " WHERE ti.idTicket='"+Codigo+"';");
             while(rs.next()){
                 Date f=rs.getDate("c.FechaCita");
-                String h=rs.getString("t.hora");
+                String h=rs.getString("tu.hora");
                 j1.setText(f.toString());
                 j2.setText(h);
             }//fin while
@@ -196,14 +195,15 @@ public class CRUD_ReservarCita extends ConectarBD{
     //metodo que recupera el dueño al label
     public void DatosRecibo(String idCi, JLabel e1, JLabel e2, JLabel e3, JLabel e4, JLabel e5, JLabel e6, JLabel e7, JLabel e8, JLabel e9){
         try{
-            rs=st.executeQuery("select p.Nombres, p.Apellidos, p.DNI, m.idMascota, m.Nombre_mascota, s.NombreServicio, v.NombresVeterinario, v.ApellidosVeterinario, c.FechaCita, t.hora, c.Precio"+
-                              " from CITA c" +
-                              " inner join propietario p on c.DNI=p.DNI"+
-                              " inner join mascota m on c.IdMascota=m.IdMascota"+
-                              " inner join servicios s on c.idServicios=s.idServicios"+
-                              " inner join veterinario v on c.idVeterinario=v.idVeterinario"+
-                              " inner join TURNO t on c.idTurno=t.idTurno"+
-                              " where c.IdCita='"+idCi+"';");
+            rs=st.executeQuery("SELECT p.Nombres, p.Apellidos, p.DNI, m.idMascota, m.Nombre_mascota, s.NombreServicio, v.NombresVeterinario, v.ApellidosVeterinario, c.FechaCita, t.hora, c.Precio"+
+                              " FROM RECIBO r" +
+                              " INNER JOIN CITA c ON r.IdCita=c.IdCita"+
+                              " INNER JOIN MASCOTA m ON c.IdMascota=m.IdMascota"+
+                              " INNER JOIN PROPIETARIO p ON m.IdPropietario=p.IdPropietario"+
+                              " INNER JOIN SERVICIOS s ON c.idServicios=s.idServicios"+
+                              " INNER JOIN VETERINARIO v ON c.idVeterinario=v.idVeterinario"+
+                              " INNER JOIN TURNO t ON c.idTurno=t.idTurno"+
+                              " WHERE r.IdCita='"+idCi+"';");
             while(rs.next()){
                 String Nombres=rs.getString("p.Nombres");
                 String Apellidos=rs.getString("p.Apellidos");
