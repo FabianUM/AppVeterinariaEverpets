bf054ec8095a2b66f2b9b2dcbe97ca7002eb2925
FabianUM <fabian.israel.07@gmail.com>
2024-11-01 23:05:19 -0500

    adjustments in the code of the pet card table

---
+++ src/Modelo/CarnetClass.java
@@ -0,0 +1,19 @@
+package Modelo;
+//librerias
+
+public class CarnetClass {
+    private int idCarnet;
+    private byte[] imagen;
+    private String idMascota;
+    public CarnetClass(){}
+
+    public int getIdCarnet()                    {return idCarnet;}
+    public void setIdCarnet(int idCarnet)       {this.idCarnet = idCarnet;}
+    public byte[] getImagen()                   {return imagen;}
+    public void setImagen(byte[] imagen)        {this.imagen = imagen;}
+    public String getIdMascota()                {return idMascota;}
+    public void setIdMascota(String idMascota)  {this.idMascota = idMascota;}
+    
+    
+    
+}//Fin clase
