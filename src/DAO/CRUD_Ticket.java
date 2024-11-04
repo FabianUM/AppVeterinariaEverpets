00a847e14d8288c2961efec8064d835f61aa1179
FabianUM <fabian.israel.07@gmail.com>
2024-11-04 18:04:01 -0500

    adjustments to the ticket listing code

---
+++ src/DAO/CRUD_Ticket.java
@@ -0,0 +1,59 @@
+package DAO;
+//librerias
+import Formatos.*;
+import Modelo.*;
+import java.util.Date;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class CRUD_Ticket extends ConectarBD{
+    public CRUD_Ticket (){}
+    
+    // Método para mostrar los tickets en un JTable
+    public void MostrarTicketEnTabla(JTable tabla, String consulta) {
+        String[] Titulos = {"Nro", "ID Ticket", "ID Cita", "Fecha", "Hora"};
+        DefaultTableModel modelo = new DefaultTableModel(null, Titulos);
+        tabla.setModel(modelo);
+        int cantreg = 0;
+
+        try {
+            // Ejecuta la consulta para obtener los registros
+            rs = st.executeQuery(consulta);
+
+            while (rs.next()) {
+                cantreg++;
+                TicketClass t = new TicketClass();
+                t.setIdTicket(rs.getInt(1));
+                t.setIdCita(rs.getString(2));
+                t.setFecha(rs.getDate(3));
+                t.setHora(rs.getString(4));
+                modelo.addRow(t.RegistroTicket(cantreg));
+            }
+            con.close();
+        } catch (Exception e) {
+            Mensajes.M1("ERROR al mostrar los tickets en el JTable: " + e);
+        }
+    }//Fin metodo
+    
+    //metodo que muestra datos en ticket
+    public TicketClass DatosTicket(int Codigo){
+        TicketClass ticket = new TicketClass();
+        try{
+            rs=st.executeQuery("SELECT c.FechaCita, tu.hora"+
+                              " FROM TICKET ti"+
+                              " INNER JOIN CITA c ON ti.idCita =c.idCita"+
+                              " INNER JOIN TURNO tu ON c.idTurno=tu.idTurno"+
+                              " WHERE ti.idTicket="+Codigo+";");
+            while(rs.next()){
+                ticket.setFecha(rs.getDate("c.FechaCita"));  
+                ticket.setHora(rs.getString("tu.hora")); 
+            }//fin while
+            con.close();
+        }catch(Exception e){
+            Mensajes.M1("ERROR no se muestran datos del ticket..."+e);
+        }
+        return ticket;
+    }//fin metodo
+    
+}//Fin clase
