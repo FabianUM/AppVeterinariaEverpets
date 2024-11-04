709fee731ce07a0f3f1f72d9a03b5949ebb929c8
FabianUM <fabian.israel.07@gmail.com>
2024-11-03 21:20:22 -0500

    adjustments in the card listing code

--- src/DAO/CRUD_Carnet.java
+++ src/DAO/CRUD_Carnet.java
@@ -3,11 +3,48 @@ package DAO;
 import Formatos.*;
 import Modelo.*;
 import com.mysql.cj.jdbc.Blob;
+import javax.swing.ImageIcon;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class CRUD_Carnet extends ConectarBD{
     public CRUD_Carnet (){}
+   
+    // Método para mostrar los carnets en un JTable
+    public void MostrarCarnetEnTabla(JTable tabla, String consulta) {
+        String[] Titulos = {"Nro", "ID Carnet", "Imagen", "ID Mascota"};
+        DefaultTableModel modelo = new DefaultTableModel(null, Titulos);
+        tabla.setModel(modelo);
+        int cantreg = 0;
+
+        try {
+            // Ejecuta la consulta para obtener los registros
+            rs = st.executeQuery(consulta);
+
+            while (rs.next()) {
+                cantreg++;
+                CarnetClass carnet = new CarnetClass();
+                carnet.setIdCarnet(rs.getInt("idCarnet"));
+                carnet.setIdMascota(rs.getString("idMascota"));
+
+                // Recupera la imagen como BLOB
+                Blob blob = (Blob) rs.getBlob("imagen");
+                if (blob != null) {
+                    byte[] imagenBytes = blob.getBytes(1, (int) blob.length());
+                    // Convierte el arreglo de bytes en un ImageIcon
+                    ImageIcon imagenIcon = new ImageIcon(imagenBytes);
+                    // Se agrega la imagen como ImageIcon en el registro
+                    modelo.addRow(new Object[]{cantreg, carnet.getIdCarnet(), imagenIcon, carnet.getIdMascota()});
+                } else {
+                    // Si no hay imagen, se puede agregar un icono vacío o nulo
+                    modelo.addRow(new Object[]{cantreg, carnet.getIdCarnet(), null, carnet.getIdMascota()});
+                }
+            }
+            con.close();
+        } catch (Exception e) {
+            Mensajes.M1("ERROR al mostrar los carnets en el JTable: " + e);
+        }
+    }//Fin metodo
     
     //metodo que inserta un registro de carnet con imagen
     public void InsertarCarnet(CarnetClass carnet) {
@@ -30,5 +67,41 @@ public class CRUD_Carnet extends ConectarBD{
             Mensajes.M1("ERROR al insertar carnet: " + e.getMessage());
         }
     }//fin metodo
+    
+    // Método para mostrar los carnets en un JTable
+    public void RecuperarCarnet(JTable tabla) {
+        String[] Titulos = {"Nro", "ID Carnet", "Imagen", "ID Mascota"};
+        DefaultTableModel modelo = new DefaultTableModel(null, Titulos);
+        tabla.setModel(modelo);
+        int cantreg = 0;
+
+        try {
+            // Ejecuta la consulta para obtener los registros
+            rs = st.executeQuery("SELECT idCarnet, idMascota, imagen FROM CARNET_MASCOTA");
+
+            while (rs.next()) {
+                cantreg++;
+                CarnetClass carnet = new CarnetClass();
+                carnet.setIdCarnet(rs.getInt("idCarnet"));
+                carnet.setIdMascota(rs.getString("idMascota"));
+
+                // Recupera la imagen como BLOB
+                Blob blob = (Blob) rs.getBlob("imagen");
+                if (blob != null) {
+                    byte[] imagenBytes = blob.getBytes(1, (int) blob.length());
+                    // Convierte el arreglo de bytes en un ImageIcon
+                    ImageIcon imagenIcon = new ImageIcon(imagenBytes);
+                    // Se agrega la imagen como ImageIcon en el registro
+                    modelo.addRow(new Object[]{cantreg, carnet.getIdCarnet(), imagenIcon, carnet.getIdMascota()});
+                } else {
+                    // Si no hay imagen, se puede agregar un icono vacío o nulo
+                    modelo.addRow(new Object[]{cantreg, carnet.getIdCarnet(), null, carnet.getIdMascota()});
+                }
+            }
+            con.close();
+        } catch (Exception e) {
+            Mensajes.M1("ERROR al mostrar los carnets en el JTable: " + e);
+        }
+    }//Fin metodo
     
 }//Fin clase
