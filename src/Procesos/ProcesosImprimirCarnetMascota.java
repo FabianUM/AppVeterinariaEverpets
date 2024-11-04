1af87d4ff025c9c89ec74a39d641734371a7202a
FabianUM <fabian.israel.07@gmail.com>
2024-11-04 00:51:33 -0500

    second adjustments in the card listing code

--- src/Procesos/ProcesosImprimirCarnetMascota.java
+++ src/Procesos/ProcesosImprimirCarnetMascota.java
@@ -14,6 +14,24 @@ public class ProcesosImprimirCarnetMascota {
         f5.txtCodigo.requestFocus();
     }
     
+    public static void BotonesAlIniciar(ImprimirCarnetMascota f5){
+        f5.btnImprimir.setEnabled(false);
+        f5.btnImprimir.setVisible(false);
+    }
+    
+    public static void BotonesAlRecuperar(ImprimirCarnetMascota f5){
+        f5.txtCodigo.setEnabled(false);
+        
+        f5.btnMostrarDatos.setEnabled(false);
+        f5.btnMostrarDatos.setVisible(false);
+        
+        f5.btnSeleccionarImagen.setEnabled(false);
+        f5.btnSeleccionarImagen.setVisible(false);
+        
+        f5.btnGenerarImprimir.setEnabled(false);
+        f5.btnGenerarImprimir.setVisible(false);
+    }
+    
     public static void LimpiarEntradasCarnet(Carnet f5){
         f5.lblCodigo.setText("");
         f5.lblDueño.setText("");
