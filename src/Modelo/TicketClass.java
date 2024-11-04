3b6b1336a21ddca9fbd1aa8a008d89cd08541aab
FabianUM <fabian.israel.07@gmail.com>
2024-11-04 18:03:47 -0500

    adjustments to the ticket listing code

---
+++ src/Modelo/TicketClass.java
@@ -0,0 +1,27 @@
+package Modelo;
+
+import java.util.Date;
+
+public class TicketClass {
+    private int idTicket;
+    private String idCita;
+    private String hora;
+    private Date fecha;
+    public TicketClass() {}
+
+    public int getIdTicket()                    {return idTicket;}
+    public void setIdTicket(int idTicket)       {this.idTicket = idTicket;}
+    public String getIdCita()                   {return idCita;}
+    public void setIdCita(String idCita)        {this.idCita = idCita;}
+    public String getHora()                     {return hora;}
+    public void setHora(String hora)            {this.hora = hora;}
+    public Date getFecha()                      {return fecha;}
+    public void setFecha(Date fecha)            {this.fecha = fecha;}
+    
+    // Método para devolver un arreglo de objetos
+    public Object[] RegistroTicket(int numeracion) {
+        return new Object[] { numeracion, idTicket, idCita, fecha, hora };
+    }
+    
+    
+}//Fin Clase
