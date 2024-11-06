98d87802f457034503cd96ab22f06a8d9b166fa6
FabianUM <fabian.israel.07@gmail.com>
2024-11-05 19:29:37 -0500

    graphical reports added

---
+++ src/Procesos/ProcesosReportes.java
@@ -0,0 +1,15 @@
+package Procesos;
+//librerias
+import DAO.*;
+import Modelo.*;
+import Vista.*;
+
+public class ProcesosReportes {
+    public static void Presentacion(FrmGraficos f15){
+        f15.setVisible(true);
+        f15.setTitle("Frm Reportes Gráficos");
+        
+    }//Fin metodo
+    
+    
+}//fin clase
