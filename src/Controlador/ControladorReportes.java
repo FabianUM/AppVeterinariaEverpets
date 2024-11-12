430bf3fbefcf3273a43e192acccc4086fcd3a3da
FabianUM <fabian.israel.07@gmail.com>
2024-11-12 12:17:55 -0500

    cleaning unnecessary imports in controller

--- src/Controlador/ControladorReportes.java
+++ src/Controlador/ControladorReportes.java
@@ -1,10 +1,7 @@
 package Controlador;
 //librerias
 import DAO.*;
-import Formatos.*;
-import Modelo.*;
 import Vista.*;
-import Procesos.*;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
