4e69ffaf35b91d67599e1fa1597c080f350d5c7f
FabianUM <fabian.israel.07@gmail.com>
2024-11-12 12:24:45 -0500

    cleaning unnecessary imports in DAO

--- src/DAO/ExportarExcel.java
+++ src/DAO/ExportarExcel.java
@@ -1,8 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
+//librerias
 import java.awt.Desktop;
 import java.io.File;
 import java.io.FileOutputStream;
